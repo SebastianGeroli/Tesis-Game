2a5d4c7cc2267d7e8a3174c2f9e72930d3124e67
Sebastian Geroli <sebastiangeroli@outlook.com>
2019-03-23 18:57:54 -0300

    cambio de version a unity mas avanzado

---
+++ RunnerCube/Assets/Scripts/MainGame.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainGame : MonoBehaviour {
+    public bool gameOver = false;
+    public void GameOverChanger()
+    {
+        if (gameOver == false)
+        {
+            gameOver = true;
+        }
+        else {
+            gameOver = false;
+        }
+    }
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            GameOverChanger();
+        }
+        if (gameOver == false)
+        {
+            Debug.Log("Playing");
+        }
+        else {
+            Debug.Log("End");
+        }
+    }
+}
---
+++ RunnerCube/Assets/Scripts/Obstacles.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacles : MonoBehaviour {
+    public float figure;
+    //Constructor base
+   public Obstacles()
+    {
+
+    }
+    public Obstacles(int a, int b)
+    {
+
+    }
+}
