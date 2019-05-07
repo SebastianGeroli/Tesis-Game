20b0b9012bb199ad8f36abbd1398d26beb78f9c4
Mauricio Rodriguez Porcel <x-tremecomputer@hotmail.com>
2019-05-07 18:32:22 -0300

    Assets Menu_UI
    Assets y cambios en la UI del Menu

---
+++ RunnerCube/Assets/Scripts/Menu_UI.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+public class Menu_UI : MonoBehaviour
+{
+    public Button btn;
+    bool encender = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Image luz = GetComponent<Image>();
+       
+        
+        EventTrigger trigger = GetComponent<EventTrigger>();
+        EventTrigger.Entry entry = new EventTrigger.Entry();
+        entry.eventID = EventTriggerType.PointerEnter;
+        entry.callback.AddListener((data) => { OnPointerEnter((PointerEventData)data); });
+        trigger.triggers.Add(entry);
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+    public void OnPointerEnter(PointerEventData data)
+    {
+        if (encender == false)
+        {
+            encender = true;
+
+        }
+        Debug.Log("OnPointerEnter called.");
+       
+    }
+  
+}
