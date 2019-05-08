e67439cfc95fdc2f2e2f3fef3df8f619ce15562d
Mauricio Rodriguez Porcel <x-tremecomputer@hotmail.com>
2019-05-07 21:10:55 -0300

    Botones Menu con Highlight
    Agregado script ui a los bonoes onpointerenter y exit para los highlights y efectos dotween

--- RunnerCube/Assets/Scripts/Menu_UI.cs
+++ RunnerCube/Assets/Scripts/Menu_UI.cs
@@ -4,24 +4,36 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using UnityEngine.Events;
+using DG.Tweening;
 
 public class Menu_UI : MonoBehaviour
 {
     public Button btn;
-    bool encender = false;
+    public Sprite luz;
+    public Sprite sinLuz;
+    Image myImage; 
+    public bool encender = false;
+    //Sequence seq = DOTween.Sequence();
 
     // Start is called before the first frame update
     void Start()
     {
-        //Image luz = GetComponent<Image>();
-       
+           
         
         EventTrigger trigger = GetComponent<EventTrigger>();
         EventTrigger.Entry entry = new EventTrigger.Entry();
         entry.eventID = EventTriggerType.PointerEnter;
         entry.callback.AddListener((data) => { OnPointerEnter((PointerEventData)data); });
         trigger.triggers.Add(entry);
-       
+
+        EventTrigger trigger2 = GetComponent<EventTrigger>();
+        EventTrigger.Entry entry2 = new EventTrigger.Entry();
+        entry2.eventID = EventTriggerType.PointerExit;
+        entry2.callback.AddListener((data) => { OnPointerExit((PointerEventData)data); });
+        trigger2.triggers.Add(entry2);
+
+        myImage = GetComponent<Image>();
+
     }
 
     // Update is called once per frame
@@ -33,12 +45,25 @@ public class Menu_UI : MonoBehaviour
     public void OnPointerEnter(PointerEventData data)
     {
         if (encender == false)
-        {
+        {           
+            btn.interactable = true;
+            btn.image.overrideSprite = luz;
+            btn.image.DOFade(0.8f, 1f);
             encender = true;
-
-        }
+         }
+       
         Debug.Log("OnPointerEnter called.");
        
     }
-  
+    public void OnPointerExit(PointerEventData data)
+    {
+        if (encender == true)
+        {
+            btn.image.DOFade(1f, 1f);
+            btn.image.overrideSprite = sinLuz;
+            encender = false;
+        }
+        Debug.Log("OnPointerExit called.");
+    }
+
 }
