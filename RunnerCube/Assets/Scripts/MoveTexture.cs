17a402d24e1d91f57ffa6eefd4b7f0dd1b873e96
Mauricio Rodriguez Porcel <x-tremecomputer@hotmail.com>
2019-06-05 20:47:16 -0300

    CircuitPath
    Agragados y configurados CircuitPath

--- RunnerCube/Assets/Scripts/MoveTexture.cs
+++ RunnerCube/Assets/Scripts/MoveTexture.cs
@@ -8,10 +8,13 @@ public class MoveTexture : MonoBehaviour
     public GameManagerRunner gameManager;
     Material material;
     float scrollSpeed = 2f;
+    public GameObject circuitPath;
+
     /*################################ Metodos  ##################################*/
     //Start
     private void Start()
     {
+        circuitPath = GetComponent<GameObject>();
         gameManager = FindObjectOfType<GameManagerRunner>();
         material = transform.GetComponent<Renderer>().material;
     }
@@ -23,10 +26,18 @@ public class MoveTexture : MonoBehaviour
     //Mover Offset material\
     public void MoveOffSet()
     {
+        if (gameObject.tag == "Layer1"){ 
         float offset = Time.time * scrollSpeed * gameManager.speeTexture;
-        material.mainTextureOffset = new Vector2 (0,offset);
-        
-      
+        material.mainTextureOffset = new Vector2 (0,offset * 0.75f);
+        }
+
+        if (gameObject.tag == "Layer2")
+        {
+            float offset = Time.time * scrollSpeed * gameManager.speeTexture;
+            material.mainTextureOffset = new Vector2(0, offset * .5f);
+        }
+
+
     }
     
 }
