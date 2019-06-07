dcdca28ed4c2fdbff46d7fb2e36505c8f58791c5
Sebastian Geroli <sebastiangeroli@outlook.com>
2019-06-07 18:31:49 -0300

    joystick
    clamp

--- RunnerCube/Assets/Scripts/JoystickMove.cs
+++ RunnerCube/Assets/Scripts/JoystickMove.cs
@@ -1,38 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[System.Serializable]
+public class Boundary{
+	public float xMin, xMax, yMin, yMax,zMin,zMax;
+}
 public class JoystickMove:MonoBehaviour {
+	public Boundary boundary;
 	public GameManagerRunner gameManagerRunner;
 	public Joystick joystick;
 	float horizontalMove;
 	float verticalMove;
 	public float sentivity;
+	public Rigidbody rb;
+	public float tiltz,tiltx;
+	private float speed = 100;
 	// Update is called once per frame
-	void Update() {
+	private void Awake() {
+		rb = gameObject.GetComponent<Rigidbody>();
+	}
+	void FixedUpdate() {
 		if( !gameManagerRunner.pause ) {
 			horizontalMove = joystick.Horizontal/sentivity;
 			verticalMove = joystick.Vertical/sentivity;
-			if( transform.position.x <= 2.5f && transform.position.x >= -2.5f && transform.position.y >= 0 && transform.position.y <= 5 ) {
-				transform.position += new Vector3( horizontalMove , verticalMove , 0 );
-			} else if( transform.position.y > 5 ) {
-				float x = transform.position.x;
-				transform.position = new Vector3( x , 5 , 0 );
-				transform.position += new Vector3( horizontalMove , 0 , 0 );
-			} else if( transform.position.y < 0 ) {
-				float x = transform.position.x;
-				transform.position = new Vector3( x , 0 , 0 );
-				transform.position += new Vector3( horizontalMove , 0 , 0 );
-			} else if( transform.position.x > 2.5f ) {
-				float y = transform.position.y;
-				transform.position = new Vector3( 2.5f , y , 0 );
-				transform.position += new Vector3( 0 , verticalMove , 0 );
-			} else if( transform.position.x < -2.5f ) {
-				float y = transform.position.y;
-				transform.position = new Vector3( -2.5f , y , 0 );
-				transform.position += new Vector3( 0 , verticalMove , 0 );
-			}
+			Vector3 movement = new Vector3( horizontalMove , verticalMove , 0f );
+			rb.velocity = movement * speed;
+				rb.position += new Vector3( horizontalMove , verticalMove , 0 );
+			rb.position = new Vector3(
+				Mathf.Clamp( rb.position.x , boundary.xMin , boundary.xMax ),
+				Mathf.Clamp( rb.position.y , boundary.yMin , boundary.yMax),
+				Mathf.Clamp( rb.position.z , boundary.zMin , boundary.zMax )
+				);
+			rb.rotation = Quaternion.Euler( rb.velocity.y * tiltx , 180 , rb.velocity.x * tiltz );
 		}
+		
+
 
 	}
 }
