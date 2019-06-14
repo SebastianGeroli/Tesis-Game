665c0789c7e29d476fd311887bf9d84bb1ff275f
Sebastian Geroli <sebastiangeroli@outlook.com>
2019-06-14 03:48:34 -0300

    sonido y mute arreglados

--- RunnerCube/Assets/Scripts/isMuted.cs
+++ RunnerCube/Assets/Scripts/isMuted.cs
@@ -4,21 +4,13 @@ using UnityEngine;
 
 public class isMuted : MonoBehaviour
 {
-	AudioSource audioSource;
-    // Start is called before the first frame update
-    void Start()
-    {
-		audioSource = GetComponent<AudioSource>();
-    }
 	private void Update() {
-		if( DataController.control.cambioSonido ) {
-			if( DataController.control.mute ) {
-				audioSource.mute = true;
-				DataController.control.cambioSonido = false;
-			} else {
-				audioSource.mute = false;
-				DataController.control.cambioSonido = false;
-			}
+		if( DataController.control.mute ) {
+			GetComponent<AudioSource>().mute = true;
+			DataController.control.cambioSonido = false;
+		} else {
+			GetComponent<AudioSource>().mute = false;
+			DataController.control.cambioSonido = false;
 		}
 	}
 }
